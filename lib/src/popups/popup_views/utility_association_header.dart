37b40d2339420146a6185284be7070438e79d736
Paul Sturm <psturm@esri.com>
2025-10-17 15:12:06 -0500

    factor out a common navigation header

---
+++ lib/src/popups/popup_views/utility_association_header.dart
@@ -0,0 +1,66 @@
+//
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+part of '../../../arcgis_maps_toolkit.dart';
+
+class _UtilityAssociationHeader extends StatelessWidget {
+  const _UtilityAssociationHeader({required this.title, this.subtitle});
+
+  final String title;
+
+  final String? subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.findAncestorStateOfType<_PopupViewState>()!;
+
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // (^) back to the initial page.
+          IconButton(
+            onPressed: state._popToRoot,
+            icon: const Icon(Icons.arrow_upward),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.titleMedium,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                if (subtitle != null)
+                  Text(
+                    subtitle!,
+                    style: Theme.of(
+                      context,
+                    ).textTheme.bodySmall?.copyWith(color: Colors.grey[600]),
+                  ),
+              ],
+            ),
+          ),
+          // (x) close the page.
+          IconButton(icon: const Icon(Icons.close), onPressed: state._pop),
+        ],
+      ),
+    );
+  }
+}
